82e56c91d0c0a8d1682b56e64bc7214c436d7a96
Vikram Mandyam <vmandyam@linkedin.com>
2020-10-27 11:41:55 +0530

    Added a O(n) way to find the maximum element in a 2D matrix

---
+++ ProgrammingAlgorithms/MaxElementInA2DMatrix/max-2d-matrix.lisp
@@ -0,0 +1,8 @@
+(defun mat-max (mat)
+  (let (max)
+    (dotimes (i (array-dimension mat 0))
+      (dotimes (j (array-dimension mat 1))
+        (when (or (null max)
+                  (> (aref mat i j) max))
+          (setq max (aref mat i j)))))
+    max)) 
